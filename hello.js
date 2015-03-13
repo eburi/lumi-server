3d61ee47af4f7dc0fbbe11c46858d34d62793d72
Erich Buri <erich.buri@gmail.com>
2015-03-13 12:59:15 +0100

    list /dev and /

--- hello.js
+++ hello.js
@@ -1,1 +1,19 @@
-console.log('servus!');
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+
+console.log('Listing /dev');
+console.log(getFiles('/dev'));
+
+console.log('Listing /');
+console.log(getFiles('/'));
