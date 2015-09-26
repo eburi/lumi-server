2ba26548d773e05b75c65d04ad3efea79ae59b27
Erich Buri <erich.buri@gmail.com>
2015-09-26 05:21:06 +0200

    hello.js: use 0x80 instead of 0xff

--- hello.js
+++ hello.js
@@ -13,13 +13,13 @@ for(var y=0; y < HEIGHT; y++) {
   for(var x=0; x < WIDTH; x++) {
     var pos = (y * WIDTH + x);
     var fPos = pos * 3;
-    evenBuffer[fPos    ] = (y % 2 === 0) ? 0x00 : 0xFF; // R
-    evenBuffer[fPos + 1] = (y % 2 === 0) ? 0x00 : 0xFF; // G
-    evenBuffer[fPos + 2] = (y % 2 === 0) ? 0x00 : 0xFF; // B
+    evenBuffer[fPos    ] = (y % 2 === 0) ? 0x00 : 0x80; // R
+    evenBuffer[fPos + 1] = (y % 2 === 0) ? 0x00 : 0x80; // G
+    evenBuffer[fPos + 2] = (y % 2 === 0) ? 0x00 : 0x80; // B
 
-    oddBuffer[fPos    ] = (y % 2 === 1) ? 0x00 : 0xFF; // R
-    oddBuffer[fPos + 1] = (y % 2 === 1) ? 0x00 : 0xFF; // G
-    oddBuffer[fPos + 2] = (y % 2 === 1) ? 0x00 : 0xFF; // B
+    oddBuffer[fPos    ] = (y % 2 === 1) ? 0x00 : 0x80; // R
+    oddBuffer[fPos + 1] = (y % 2 === 1) ? 0x00 : 0x80; // G
+    oddBuffer[fPos + 2] = (y % 2 === 1) ? 0x00 : 0x80; // B
   }
 }
 
