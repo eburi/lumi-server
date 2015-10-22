fd47b7b70eb02491341e564ce5cd37f3524b4cc3
Erich Buri <erich.buri@gmail.com>
2015-10-22 03:12:38 +0200

    silence lumi

--- webapp/app/js/utils/lumi.js
+++ webapp/app/js/utils/lumi.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 
 console.log('this is the lumi-module...');
 
-var socket = io();
-socket.on('connect', function() { console.log('connected'); });
-socket.on('event', function(data) { console.log('socket-event: ', data); });
-socket.on('disconnect', function() { console.log('disconnect'); });
+// var socket = io();
+// socket.on('connect', function() { console.log('connected'); });
+// socket.on('event', function(data) { console.log('socket-event: ', data); });
+// socket.on('disconnect', function() { console.log('disconnect'); });
