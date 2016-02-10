f29fe866a64d671279849c65594a76a0ef35a712
Erich Buri <erich.buri@gmail.com>
2016-02-10 22:25:00 +0100

    added latest resin.io experiments

--- deps.sh
+++ deps.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -o errexit
 
 ln -s ./bin/phantomjs /bin/
 
 echo "linked?"
 echo "ls -l /bin/phantom*.."
 ls -l /bin/phantom*
+
+# Install dropbear
+apt-get -q update
+apt-get install -y dropbear
