823d72742bacff8dc574c5d15689dc12e4b7a7a3
Erich Buri <erich.buri@gmail.com>
2016-02-11 01:49:25 +0100

    Revert "removed skeleton react-app"
    This reverts commit b52ad2ae44570b7a10f2714d64d8ea4fdabd4a0d.

---
+++ src/config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var fs = require('fs')
+  , cache; //cache parsed config object
+
+function config () {
+  if (cache) {
+    return cache;
+  }
+
+  var path = process.cwd() + '/config.json'
+    , content;
+
+  //load and merge object
+  content = fs.readFileSync(path, 'utf-8');
+
+  try {
+    cache = JSON.parse(content);
+    console.info('config file ' + path + ' loaded.');
+  } catch(e) {
+    console.error('could not parse config file: ' + path);
+    throw e;
+  }
+
+  return cache;
+}
+
+module.exports = config;
---
+++ src/main.js
@@ -0,0 +1,9 @@
+'use strict';
+/** @jsx React.DOM */
+
+var React = require('react/addons');
+var ReactApp = require('./components/ReactApp');
+
+var mountNode = document.getElementById('react-main-mount');
+
+React.render(new ReactApp({}), mountNode);
