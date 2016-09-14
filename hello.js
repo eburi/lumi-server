9fb913bebe0f468fa9198ad80f7262fae19b7bf8
Erich Buri <erich.buri@gmail.com>
2016-09-14 23:49:14 +0200

    changed lumi-path

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var lumiDevice = require('./lib/lumi_raspberrypi.js');
+var lumiDevice = require('lumi/lumi_raspberrypi.js');
 lumiDevice.openPort();
 
 var WIDTH = 32;
@@ -9,16 +9,16 @@ var HEIGHT = 32;
 var evenBuffer = new Buffer(WIDTH * HEIGHT * 3);
 var oddBuffer = new Buffer(WIDTH * HEIGHT * 3);
 
-for(var y=0; y < HEIGHT; y++) {
-  for(var x=0; x < WIDTH; x++) {
+for (var y = 0; y < HEIGHT; y++) {
+  for (var x = 0; x < WIDTH; x++) {
     var pos = (y * WIDTH + x);
     var fPos = pos * 3;
     var oddLine = (y % 2 === 0);
-    evenBuffer[fPos    ] = !oddLine ? 0x00 : 0x80; // R
+    evenBuffer[fPos] = !oddLine ? 0x00 : 0x80; // R
     evenBuffer[fPos + 1] = !oddLine ? 0x00 : 0x00; // G
     evenBuffer[fPos + 2] = !oddLine ? 0x00 : 0x00; // B
 
-    oddBuffer[fPos    ] = oddLine ? 0x00 : 0x80; // R
+    oddBuffer[fPos] = oddLine ? 0x00 : 0x80; // R
     oddBuffer[fPos + 1] = oddLine ? 0x00 : 0x00; // G
     oddBuffer[fPos + 2] = oddLine ? 0x00 : 0x00; // B
   }
